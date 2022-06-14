858e829681a43aa00af941349861ed02894f6bb4
Your Name <kalaitzidispaul@gmail.com>
2022-06-14 18:43:34 -0400

    added heroku mongo

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -20,6 +20,7 @@ const SavedBooks = () => {
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
+    
     const token = Auth.loggedIn() ? Auth.getToken() : null
 
     if (!token) {
